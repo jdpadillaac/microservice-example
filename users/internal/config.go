1b10667180f95d7a150021cf6bd71328abe9ce1f
Jonatan Padilla <jdpadill@bancolombia.com.co>
2021-08-22 11:49:03 -0500

    app config model

--- users/internal/config.go
+++ users/internal/config.go
@@ -3,9 +3,13 @@ package internal
 import "os"
 
 type AppConfig struct {
-	Port string
+	Port    string
+	BaseUrl string
 }
 
 func NewAppConfig() *AppConfig {
-	return &AppConfig{Port: os.Getenv("PORT")}
+	return &AppConfig{
+		Port:    os.Getenv("PORT"),
+		BaseUrl: os.Getenv("BASE_URL"),
+	}
 }
